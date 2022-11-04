65596fbb7053be059972f658ce5ae0f15fa504aa
vinimedeiros13 <vinimedeiros7@outlook.com>
2022-11-04 09:17:56 -0300

    feat: add repo

---
+++ src/screens/SignIn.tsx
@@ -0,0 +1,9 @@
+import { Center, Heading } from "native-base";
+
+export const SignIn = () => {
+  return (
+    <Center flex="1" bg="gray.950">
+      <Heading color="gray.200">Oi</Heading>
+    </Center>
+  );
+};
